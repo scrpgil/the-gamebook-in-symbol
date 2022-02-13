972a822bc50364f6a6d1a5f0f75f9e4fd95b7291
scrpgil <scraplbuild@gmail.com>
2022-02-14 03:09:21 +0900

    change

---
+++ services/const.prod.ts
@@ -0,0 +1,8 @@
+/** TESTNETのURL */
+export const NODE_URL = 'https://sym-main.opening-line.jp:3001';
+
+/** 番地用アドレス */
+export const NUMBERING_ADDRESS = 'NC3F3E2EZT4JRPLUSYMD2QHWKW353GJUBP2NSPA';
+
+/** Explorer */
+export const SYMBOL_EXPLORER = 'https://symbol.fyi/';
---
+++ services/const.staging.ts
@@ -0,0 +1,8 @@
+/** TESTNETのURL */
+export const NODE_URL = 'https://sym-test-01.opening-line.jp:3001';
+
+/** 番地用アドレス */
+export const NUMBERING_ADDRESS = 'TD6Y6MAG4AR2CMWJWQ3DZP3PT5QZ74TDUTNSKPY';
+
+/** Explorer */
+export const SYMBOL_EXPLORER = 'https://testnet.symbol.fyi/';
