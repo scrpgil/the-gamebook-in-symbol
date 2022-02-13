0798b82791861b613dfcc5ccad2500bd4a0b81b3
scrpgil <scraplbuild@gmail.com>
2022-02-14 01:39:37 +0900

    change

--- services/symbol.ts
+++ services/symbol.ts
@@ -14,9 +14,12 @@ import {
 import { RawNumberingAddress } from './gamebook';
 /** TESTNETのURL */
 // export const NODE_URL = "https://sym-test-01.opening-line.jp:3001";
-export const NODE_URL = 'https://sym-test-02.opening-line.jp:3001';
+// export const NODE_URL = 'https://sym-test-02.opening-line.jp:3001';
+export const NODE_URL = 'https://sym-main.opening-line.jp:3001';
+
 /** 番地用アドレス */
-export const NUMBERING_ADDRESS = 'TD6Y6MAG4AR2CMWJWQ3DZP3PT5QZ74TDUTNSKPY';
+// export const NUMBERING_ADDRESS = 'TD6Y6MAG4AR2CMWJWQ3DZP3PT5QZ74TDUTNSKPY';
+export const NUMBERING_ADDRESS = 'NC3F3E2EZT4JRPLUSYMD2QHWKW353GJUBP2NSPA';
 
 export const getAllTransaction = (
   rawAddress: string = NUMBERING_ADDRESS,
