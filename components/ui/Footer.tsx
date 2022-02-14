ff364c28ecd21ad6a69864a85c338664f2520ae1
scrpgil <scraplbuild@gmail.com>
2022-02-14 23:50:56 +0900

    change

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -5,13 +5,29 @@ const Footer = (props) => {
   return (
     <div className="text-white text-center text-sm py-1">
       <span
-        className="border-b mr-2"
+        className="border-b mr-2 cursor-pointer	"
         onClick={() => {
           let url = SYMBOL_EXPLORER + 'accounts/' + readAddress?.address;
           window.open(url, '_blank');
         }}
       >
         explorer
+      </span>
+      <span
+        className="border-b mr-2 cursor-pointer	"
+        onClick={() => {
+          location.hash = '#%E7%99%BB%E9%8C%B2(regist)';
+        }}
+      >
+        regist
+      </span>
+      <span
+        className="border-b mr-2 cursor-pointer	"
+        onClick={() => {
+          location.hash = '#%E6%9B%B8%E3%81%8D%E8%BE%BC%E3%81%BF(write)';
+        }}
+      >
+        write
       </span>
       <span>
         created by 2022.{' '}
